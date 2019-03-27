652827ea682900e54f0c069f4974341c08c0f4aa
dunbar-brett <dunbar.brett@gmail.com>
2019-03-26 23:10:21 -0700

    created navbar and gallery components. removed some of nuxts premade shit.

---
+++ shelly_web/components/Gallery.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Show me those pics</div>
+</template>
+
+<script>
+export default {};
+</script>
+<style>
+</style>
+
---
+++ shelly_web/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>HELLO,</h1>
+        <h2>I will be your nav bar someday</h2>
+    </div>
+</template>
+<style>
+</style>
+<script>
+export default {};
+</script>
