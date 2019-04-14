c89ab2f04bea62759bd50f05078165e205bc05eb
dunbar-brett <dunbar.brett@gmail.com>
2019-04-14 15:32:57 -0700

    redoing those tar fixes, and testing a new nav bar

--- shelly_web/components/TheHeader.vue
+++ shelly_web/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="main-header">
+    <!-- <header class="main-header">
         <nav>
             <nuxt-link to='/'
                        tag='img'
@@ -18,6 +18,38 @@
                 <li><a href="https://society6.com/shellyweasel">Store</a></li>
                 <nuxt-link to='/about' tag="li"><a>Contact</a></nuxt-link>
             </ul>
+        </nav>
+    </header> -->
+
+    <header class="main-header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="d-flex flex-grow-1">
+                <span class="w-100 d-lg-none d-block"><!-- hidden spacer to center brand on mobile --></span>
+                <a class="navbar-brand" href="#">
+                    Navbar 7
+                </a>
+                <div class="w-100 text-right">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar7">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </div>
+            <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar7">
+                <ul class="navbar-nav ml-auto flex-nowrap">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Link</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
 </template>
