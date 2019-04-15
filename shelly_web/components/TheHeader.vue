d3736619da591b261348ad858b0d28d442115026
dunbar-brett <dunbar.brett@gmail.com>
2019-04-14 17:29:12 -0700

    major changes to header and hopefully a stable dev environment

--- shelly_web/components/TheHeader.vue
+++ shelly_web/components/TheHeader.vue
@@ -8,36 +8,34 @@
                        :src='logo'
                        class="navbar-brand logo">
                 </nuxt-link>
-
                 <div class="w-100 text-right">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar7">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                 </div>
             </div>
-            <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar7">
+            <div class="collapse navbar-collapse flex-grow-1 text-center" id="myNavbar7">
                 <ul class="navbar-nav ml-auto flex-nowrap">
-                    <li class="nav-item dropdown">
-                        <a data-toggle="dropdown" class="nav-link dropdown-toggle"
-                           href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    <li class="nav-item sjw-nav-item sjw-dropdown">
+                        <a class="nav-link sjw-dropbtn">
                             Portfolio
                         </a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Paintings</a>
-                            <a class="dropdown-item" href="#">Illustrations</a>
+                        <div class="sjw-dropdown-content">
+                            <a class="sjw-dropdown-content-item text-center" href="#">Paintings</a>
+                            <a class="sjw-dropdown-content-item text-center" href="#">Illustrations</a>
                         </div>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item sjw-nav-item">
                         <a href="#" class="nav-link">About</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item sjw-nav-item">
                         <a href="https://society6.com/shellyweasel" 
                            target="_blank"
                            class="nav-link">
                            Store
                         </a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item sjw-nav-item">
                         <a href="#" class="nav-link">Contact</a>
                     </li>
                 </ul>
@@ -57,11 +55,50 @@
             height: 25%
         }
 
+        .sjw-nav-item {
+            width: 8rem;
+        }
+
+        .sjw-dropdown {
+            &:hover {
+                background: rgba(145, 216, 221, .8);
+            }
+        }
+
+        .sjw-dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: rgba(145, 216, 221, .8);
+            min-width: 8rem;
+            //box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+
+            .sjw-dropdown-content-item {
+                float: none;
+                color: black;
+                padding: 12px 16px;
+                text-decoration: none;
+                display: block;
+                text-align: left;
+
+                &:hover {
+                    //background-color: rgba(145, 216, 221, .8);
+                    color: white;
+                }
+            }
+        }
+
+        .sjw-dropdown:hover .sjw-dropdown-content {
+            display: block;
+        }
+
         .social-media-icons {
             background-color: lightgrey;
+            min-width: 12rem;
 
             .social-media-icon {
                 display: inline-block;
+                min-width: 4rem;
             }
         }
     }
